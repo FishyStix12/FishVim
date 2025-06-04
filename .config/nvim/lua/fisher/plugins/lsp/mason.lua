fce8a7f6b695ba07ee07a520afcb34f16668587a
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2025-06-03 23:34:10 -0400

    Update mason.lua

--- .config/nvim/lua/fisher/plugins/lsp/mason.lua
+++ .config/nvim/lua/fisher/plugins/lsp/mason.lua
@@ -3,6 +3,9 @@ return {
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
+    "hrsh7th/nvim-cmp",            -- completion engine
+    "hrsh7th/cmp-nvim-lsp",        -- LSP completion source
+    "L3MON4D3/LuaSnip",            -- snippet engine
     "jose-elias-alvarez/null-ls.nvim",
     "williamboman/mason-null-ls.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -13,7 +16,9 @@ return {
     local mason_null_ls = require("mason-null-ls")
     local mason_tool_installer = require("mason-tool-installer")
     local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
+    -- Setup Mason
     mason.setup()
 
     mason_lspconfig.setup({
@@ -24,19 +29,13 @@ return {
       automatic_installation = true,
     })
 
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    -- LSP capabilities (for autocompletion)
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
+    -- Basic server setup
     local servers = {
-      "pyright",
-      "lua_ls",
-      "tsserver",
-      "html",
-      "cssls",
-      "tailwindcss",
-      "svelte",
-      "graphql",
-      "emmet_ls",
-      "bashls",
+      "pyright", "lua_ls", "tsserver", "html", "cssls",
+      "tailwindcss", "svelte", "graphql", "emmet_ls", "bashls",
     }
 
     for _, server in ipairs(servers) do
@@ -45,15 +44,21 @@ return {
       })
     end
 
+    -- Mason Null-LS setup
     mason_null_ls.setup({
-      ensure_installed = { "prettier", "stylua", "eslint_d", "black" },
+      ensure_installed = {
+        "prettier", "stylua", "eslint_d", "black", "shfmt",
+        "clang_format", "shellcheck",
+      },
     })
 
+    -- Auto-install formatters and linters
     mason_tool_installer.setup({
-      ensure_installed = { "prettier", "stylua", "eslint_d", "black" },
+      ensure_installed = {
+        "prettier", "stylua", "eslint_d", "black", "shfmt", "shellcheck"
+      },
       auto_update = true,
       run_on_start = true,
     })
   end,
 }
-
